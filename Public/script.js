d063bcb29b8fada0d6cd21c35ec325a79d525c4b
Pallavi Sharma <82016181+pallavii24@users.noreply.github.com>
2023-09-28 17:29:42 +0530

    Delete Public directory

--- Public/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-
-    btn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('open')
-});
